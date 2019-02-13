5ace2ae89d2c9240610086580ec874f1a8ec22e7
Matt Allinson <mrallinson@gmail.com>
2019-02-13 23:47:00 +0200

    adds bobdole-inator.py

---
+++ bobdole.py
@@ -0,0 +1,52 @@
+import csv 
+
+def word_length_list_maker(number_of_letters, words):
+	right_length_words = []
+	for word in words:
+		if len(word) == number_of_letters:
+			right_length_words.append(word)
+
+	return right_length_words
+
+def intersecting_words(intersections, number_of_letters, words):
+    check_list = []
+    word_list = word_length_list_maker(number_of_letters, words)
+    print(len(word_list))
+    for word in word_list:
+        check_list.append([word, [word[i] for i in intersections]])
+    
+    matches = []
+    for c in check_list:
+        trial = [c[0]]
+        inters = c[1]
+        for c in check_list:
+            if c[1] == inters and c[0] not in trial:
+                trial.append(c[0])
+        if len(trial)>1 and sorted(trial) not in matches:
+            matches.append(sorted(trial))
+            print(sorted(trial))
+    
+    return matches
+
+def main():
+	with open('dictionary.txt', 'r') as f:
+	    words = [line.rstrip() for line in f]
+	    print(len(words), 'words in dictionary')
+
+	number_of_letters = int(input('how many letters should the word be>'))
+	intersections = [int(i)-1 for i in input(
+		'''which letters should match? (please enter comma separated digits)>''').split(',')]
+
+	iw = intersecting_words(intersections, number_of_letters, words)
+	
+	with open('output.tsv','w', newline='') as o:
+		csvwriter = csv.writer(o, delimiter='\t', lineterminator='\n')
+		for i in iw:
+			csvwriter.writerow(i)
+
+if __name__ == '__main__':
+	main()
+
+
+
+
