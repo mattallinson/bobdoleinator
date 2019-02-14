6687ac4b6e0b873ccb7da60b651006d530fa4bb1
Allinson Matthew <mrallinson@gmail.com>
2019-02-14 13:12:15 +0200

    adds ability to set desired letters

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-`bobdole.py` is a script that can go through a given text file of words and return sets of words that are the same length that have the same matching characters. For example a querey of _8 letter words that have the same 1st, 5th, 8th letter_ would return  `["Escapism", "Europium"]`among many, many other combinations. 
+`bobdole.py` is a script that can go through a given text file of words and return sets of words that are the same length that have the same matching characters. For example a querey of _8 letter words that have the same 1st and 5th letter with the last letter being "m"_ would return  `["Escapism", "Europium"]`among many, many other combinations. 
 
 #Usage:
 
@@ -9,10 +9,11 @@
 1. Open terminal window at folder and run `python bobdole.py`
 
 2. Enter the number of letters you want when prompted (i.e. for the above example enter >8)
-3. Enter the number of the letters you want to "intersect" separated by commas (i.e. for the above example entr >1,5,8)
+3. Fix any letters you want (i.e. for the above example enter >8:m)
+4. Enter the number of the letters you want to "intersect" separated by commas (i.e. for the above example entr >1,5)
 
-4. ????
-5. When it finishes, look back in the folder you just ran the program from, you should have a file called "output81-5-8.tsv". Open this in excel or whatever editor you wish.
+5. ????
+6. When it finishes, look back in the folder you just ran the program from, you should have a file called "output_8_1-5.tsv". Open this in excel or whatever editor you wish.
 
 *Note*
 There is no error handling built in to this, so if you ask for the 9th letter of 6 letter words, it will crash. If you separate your desired number of intersections with anything other than commas, it will crash. If you enter anything that isn't a numeric number, it will crash. It will probably also crash for many other mysterious reasons. Let me know them.
--- bobdole.py
+++ bobdole.py
@@ -1,53 +1,60 @@
 import csv 
 
-def filename_maker(number_of_letters, intersections):
-	filename = "output_" + str(number_of_letters) + '_' + "-".join(
-			[str(i+1) for i in intersections]) + ".tsv"
-	return filename
-
-def word_length_list_maker(number_of_letters, words):
-	right_length_words = []
-	for word in words:
-		if len(word) == number_of_letters:
-			right_length_words.append(word)
-
-	return right_length_words
-
 def intersecting_words(intersections, number_of_letters, words):
     check_list = []
-    word_list = word_length_list_maker(number_of_letters, words)
-    print(len(word_list))
-    for word in word_list:
-        check_list.append([word, [word[i] for i in intersections]])
+    for word in words: 
+        check_list.append([word, [word[i] for i in intersections]]) # so "spam" with intersections [1,3,4] would be >> ["spam", ["s","a","m"]]
     
     matches = []
-    for c in check_list:
-        trial = [c[0]]
-        inters = c[1]
-        for c in check_list:
-            if c[1] == inters and c[0] not in trial:
-                trial.append(c[0])
-        if len(trial)>1 and sorted(trial) not in matches:
+    for c in check_list: #["spam", ["s","a","m"]
+        trial = [c[0]] #"spam"
+        inters = c[1] #["s","a", "m"]
+        for c in check_list: #["slam", ["s","a","m"]
+            if c[1] == inters and c[0] not in trial: #True
+                trial.append(c[0]) #trial = ["spam", "slam"]
+        if len(trial)>1 and sorted(trial) not in matches: 
+        #This loop prevents ["spam", "slam"] and ["slam", "spam"] both being added to the "matches" list
+        #This is highly simple but exceedingly inefficient and should probably be replaced
             matches.append(sorted(trial))
-            print(sorted(trial))
+            print(sorted(trial)) #bit of output so we've got something to watch as it does its thing
     
     return matches
 
 def main():
-	with open('dictionary.txt', 'r') as dictionary:
-	    words = [line.rstrip() for line in dictionary]
-	    print(len(words), 'words in dictionary.txt')
+	with open('dictionary.txt', 'r') as dictionary_file: #get the words
+	    dictionary = [line.rstrip() for line in dictionary_file]
+	    print(len(dictionary), 'words in dictionary.txt') #output to test it's worked
 
+	#request number of letters from user
+	number_of_letters = int(input('how many letters should the word be> ').rstrip())
+	
+	#get set letters from user
+	required_letters_input = input('which letters are fixed? Enter in the format "position:letter" and leave black if none> ').rstrip()
+	
+	if required_letters_input == '':
+		required_letters = None
+	else:
+		required_letters = {int(position)-1:letter for position, letter in [pair.split(':') for pair in required_letters_input.split(',')]}
 
-	number_of_letters = int(input('how many letters should the word be>'))
-	intersections = [int(i)-1 for i in input(
-		'''which letters should match? (please enter comma separated digits)>''').split(',')]
-	filename = filename_maker(number_of_letters, intersections)
+	#request desired intersections from user
+	intersections = [int(i)-1 for i in input('which other letters should match? (please enter comma separated digits)> ').split(',')] 
+		
+	
 
-	iw = intersecting_words(intersections, number_of_letters, words)
+	right_length_words = [word for word in dictionary if len(word) == number_of_letters] #selects words from the dictionary that are the right length
+	
+	if required_letters is not None:
+		#selects only letters that have the required letters at the desired positions
+		words_to_test = [word for word in right_length_words if all(word[i] == required_letters[i] for i in required_letters)]
+	else:
+		words_to_test = right_length_words
+
+	filename = "output_" + str(number_of_letters) + '_' + "-".join([str(i+1) for i in intersections]) + ".tsv"
+	#the main event: get the matches list for the users inputs
+	iw = intersecting_words(intersections, number_of_letters, words_to_test) 
 	
 	
-	with open(filename,'w', newline='') as o:
+	with open(filename,'w', newline='') as o: #saves the file
 		csvwriter = csv.writer(o, delimiter='\t', lineterminator='\n')
 		for i in iw:
 			csvwriter.writerow(i)
